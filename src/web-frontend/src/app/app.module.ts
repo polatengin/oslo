a55c35bfa5b87e6d6b2b340e09195e7bb76353a1
Engin Polat <enpolat@microsoft.com>
2019-10-12 12:58:16 +0000

    common and httpclient modules added to appmodule

--- src/web-frontend/src/app/app.module.ts
+++ src/web-frontend/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    CommonModule,
     AppRoutingModule
   ],
   providers: [],
