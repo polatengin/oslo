7fbedea1f5b9e0e01774925208fd1ca0b1b946d3
Engin Polat <enpolat@microsoft.com>
2019-10-12 12:57:15 +0000

    home, products and users pages added to route list

--- src/web-frontend/src/app/app-routing.module.ts
+++ src/web-frontend/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { ProductsComponent } from './products/products.component';
+import { HomeComponent } from './home/home.component';
+import { UsersComponent } from './users/users.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: 'users', component: UsersComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
