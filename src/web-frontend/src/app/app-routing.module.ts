33b02493be2cfd4e1bbf47981839b20b36730d9f
Engin Polat <enpolat@microsoft.com>
2019-10-13 19:18:21 +0000

    productdetail component added to route list

--- src/web-frontend/src/app/app-routing.module.ts
+++ src/web-frontend/src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { ProductsComponent } from './products/products.component';
 import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { UsersComponent } from './users/users.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'products', component: ProductsComponent },
+  { path: 'product/detail', component: ProductDetailComponent },
   { path: 'users', component: UsersComponent }
 ];
 
